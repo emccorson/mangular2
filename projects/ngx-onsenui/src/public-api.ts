d7ea40a3ca76079dc2da0476b4c74b022011aa8d
Eric Corson <emccorson@gmail.com>
2020-03-03 05:55:11 +0000

    add OnsenModule

--- projects/ngx-onsenui/src/public-api.ts
+++ projects/ngx-onsenui/src/public-api.ts
@@ -2,6 +2,8 @@
  * Public API Surface of ngx-onsenui
  */
 
-export * from './lib/ngx-onsenui.service';
-export * from './lib/ngx-onsenui.component';
-export * from './lib/ngx-onsenui.module';
+//export * from './lib/ngx-onsenui.service';
+//export * from './lib/ngx-onsenui.component';
+//export * from './lib/ngx-onsenui.module';
+
+export * from './lib/onsen.module';
