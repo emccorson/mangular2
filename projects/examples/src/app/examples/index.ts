a1faceacfc96f9153795a210126f8827b945ff35
Eric Corson <emccorson@gmail.com>
2020-04-03 23:15:26 +0100

    add rest of examples to index

--- projects/examples/src/app/examples/index.ts
+++ projects/examples/src/app/examples/index.ts
@@ -1,7 +1,77 @@
 import { Example } from './example';
 
+import { ActionSheetComponent } from './action-sheet/action-sheet.component';
+import { AlertDialogComponent } from './alert-dialog/alert-dialog.component';
+import { BackButtonComponent } from './back-button/back-button.component';
+import { BottomToolbarComponent } from './bottom-toolbar/bottom-toolbar.component';
+import { ButtonComponent } from './button/button.component';
+import { CardComponent } from './card/card.component';
+import { CarouselComponent } from './carousel/carousel.component';
+import { CheckboxComponent } from './checkbox/checkbox.component';
+import { DialogComponent } from './dialog/dialog.component';
+import { FabComponent } from './fab/fab.component';
+import { GestureDetectorComponent } from './gesture-detector/gesture-detector.component';
+import { GridComponent } from './grid/grid.component';
+import { IconComponent } from './icon/icon.component';
+import { IfComponent } from './if/if.component';
+import { InputComponent } from './input/input.component';
+import { LazyRepeatComponent } from './lazy-repeat/lazy-repeat.component';
+import { ListComponent } from './list/list.component';
+import { ModalComponent } from './modal/modal.component';
 import { NavigatorComponent } from './navigator/navigator.component';
+import { NotificationComponent } from './notification/notification.component';
+import { PlatformComponent } from './platform/platform.component';
+import { PopoverComponent } from './popover/popover.component';
+import { ProgressComponent } from './progress/progress.component';
+import { PullHookComponent } from './pull-hook/pull-hook.component';
+import { RadioComponent } from './radio/radio.component';
+import { RangeComponent } from './range/range.component';
+import { RippleComponent } from './ripple/ripple.component';
+import { SearchInputComponent } from './search-input/search-input.component';
+import { SegmentComponent } from './segment/segment.component';
+import { SelectComponent } from './select/select.component';
+import { SpeedDialComponent } from './speed-dial/speed-dial.component';
+import { SplitterComponent } from './splitter/splitter.component';
+import { SwitchComponent } from './switch/switch.component';
+import { TabbarComponent } from './tabbar/tabbar.component';
+import { ToastComponent } from './toast/toast.component';
+import { ToolbarButtonComponent } from './toolbar-button/toolbar-button.component';
 
 export const examples: Example[] = [
-  { title: 'Navigator', component: NavigatorComponent }
+  { title: 'Action Sheet', component: ActionSheetComponent },
+  { title: 'Alert Dialog', component: AlertDialogComponent },
+  { title: 'Back Button', component: BackButtonComponent },
+  { title: 'Bottom Toolbar', component: BottomToolbarComponent },
+  { title: 'Button', component: ButtonComponent },
+  { title: 'Card', component: CardComponent },
+  { title: 'Carousel', component: CarouselComponent },
+  { title: 'Checkbox', component: CheckboxComponent },
+  { title: 'Dialog', component: DialogComponent },
+  { title: 'Fab', component: FabComponent },
+  { title: 'Gesture Detector', component: GestureDetectorComponent },
+  { title: 'Grid', component: GridComponent },
+  { title: 'Icon', component: IconComponent },
+  { title: 'If', component: IfComponent },
+  { title: 'Input', component: InputComponent },
+  { title: 'Lazy Repeat', component: LazyRepeatComponent },
+  { title: 'List', component: ListComponent },
+  { title: 'Modal', component: ModalComponent },
+  { title: 'Navigator', component: NavigatorComponent },
+  { title: 'Notification', component: NotificationComponent },
+  { title: 'Platform', component: PlatformComponent },
+  { title: 'Popover', component: PopoverComponent },
+  { title: 'Progress', component: ProgressComponent },
+  { title: 'PullHook', component: PullHookComponent },
+  { title: 'Radio', component: RadioComponent },
+  { title: 'Range', component: RangeComponent },
+  { title: 'Ripple', component: RippleComponent },
+  { title: 'Search Input', component: SearchInputComponent },
+  { title: 'Segment', component: SegmentComponent },
+  { title: 'Select', component: SelectComponent },
+  { title: 'Speed Dial', component: SpeedDialComponent },
+  { title: 'Splitter', component: SplitterComponent },
+  { title: 'Switch', component: SwitchComponent },
+  { title: 'Tabbar', component: TabbarComponent },
+  { title: 'Toast', component: ToastComponent },
+  { title: 'Toolbar Button', component: ToolbarButtonComponent }
 ];
